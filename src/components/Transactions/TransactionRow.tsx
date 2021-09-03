0ca6be7ff7e0e574c0b7f2ab9bc764a4ca46882e
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2021-09-03 12:07:16 -0600

    feat: wire up tx details; add error handling

--- src/components/Transactions/TransactionRow.tsx
+++ src/components/Transactions/TransactionRow.tsx
@@ -1,12 +1,28 @@
-import { PhoneIcon } from '@chakra-ui/icons'
-import { Box, Center, Collapse, Flex, Heading, useColorModeValue } from '@chakra-ui/react'
+import { CheckCircleIcon, PhoneIcon, WarningTwoIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Center,
+  Collapse,
+  Flex,
+  Link,
+  SimpleGrid,
+  Tag,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { Row } from 'components/Row'
 import { RawText, Text } from 'components/Text'
-import { FormatTransactionType, TransactionStatusEnum } from 'hooks/useTransactions/useTransactions'
+import {
+  FormatTransactionType,
+  TxStatusEnum,
+  TxTypeEnum
+} from 'hooks/useTransactions/useTransactions'
 import { useState } from 'react'
 
 export const TransactionRow = ({ tx }: { tx: FormatTransactionType }) => {
   const [isOpen, setIsOpen] = useState(false)
   const toggleOpen = () => setIsOpen(!isOpen)
+  const sentTx = tx.type === TxTypeEnum.Sent
+
   return (
     <Box
       as='button'
@@ -23,21 +39,82 @@ export const TransactionRow = ({ tx }: { tx: FormatTransactionType }) => {
           <Center w='10' h='10' bg={'whiteAlpha.200'} rounded='full' mr='3'>
             <PhoneIcon />
           </Center>
-          <Text
-            translation={
-              tx.type === TransactionStatusEnum.Sent
-                ? 'transactionRow.sent'
-                : 'transactionRow.received'
-            }
-          />
+          <Text translation={sentTx ? 'transactionRow.sent' : 'transactionRow.received'} />
           <RawText ml={2}>{`${tx.amount} ${tx.symbol}`}</RawText>
         </Flex>
-        <RawText>{`${tx.date}`}</RawText>
+        <RawText>{tx.dateFromNow}</RawText>
       </Flex>
       <Collapse in={isOpen}>
-        <Heading>
-          <Text translation={'transactionRow.moreDetails'} />
-        </Heading>
+        <SimpleGrid gridTemplateColumns='repeat(auto-fit, minmax(180px, 1fr))' spacing='4' py={6}>
+          <Row variant='vertical'>
+            <Row.Label>
+              <Text translation='transactionRow.date' />
+            </Row.Label>
+            <Row.Value>{tx.date}</Row.Value>
+          </Row>
+          <Row variant='vertical'>
+            <Row.Label>
+              <Text translation='transactionRow.txid' />
+            </Row.Label>
+            <Row.Value>
+              <Link isExternal color='blue.500'>
+                {/* TODO: add ellipsis */}
+                {`${tx.txid?.substr(0, 7)}...`}
+              </Link>
+            </Row.Value>
+          </Row>
+          <Row variant='vertical'>
+            <Row.Label>
+              <Text translation='transactionRow.fee' />
+            </Row.Label>
+            {/* TODO: make fee coin symbol denomination dynamic */}
+            <Row.Value>{`${tx.fee} ETH`}</Row.Value>
+          </Row>
+          <Row variant='vertical'>
+            <Row.Label>
+              <Text translation='transactionRow.status' />
+            </Row.Label>
+            <Row.Value textAlign='left'>
+              {tx.status === TxStatusEnum.Confirmed && (
+                <Tag colorScheme='green' size='lg'>
+                  <CheckCircleIcon mr={2} />
+                  <Text translation='transactionRow.confirmed' />
+                </Tag>
+              )}
+              {tx.status === TxStatusEnum.Failed && (
+                <Tag colorScheme='red' size='lg'>
+                  <WarningTwoIcon mr={2} />
+                  <Text translation='transactionRow.failed' />
+                </Tag>
+              )}
+            </Row.Value>
+          </Row>
+          <Row variant='vertical'>
+            <Row.Label>
+              <Text translation='transactionRow.network' />
+            </Row.Label>
+            <Row.Value>{tx.network}</Row.Value>
+          </Row>
+          <Row variant='vertical'>
+            <Row.Label>
+              <Text translation='transactionRow.blockHeight' />
+            </Row.Label>
+            <Row.Value>{tx.blockHeight}</Row.Value>
+          </Row>
+          <Row variant='vertical'>
+            <Row.Label>
+              {sentTx ? (
+                <Text translation='transactionRow.to' />
+              ) : (
+                <Text translation='transactionRow.from' />
+              )}
+            </Row.Label>
+            {/* TODO: add ellipsis */}
+            <Row.Value>
+              {sentTx ? `${tx.to?.substr(0, 7)}...` : `${tx.from?.substr(0, 7)}...`}
+            </Row.Value>
+          </Row>
+        </SimpleGrid>
       </Collapse>
     </Box>
   )
